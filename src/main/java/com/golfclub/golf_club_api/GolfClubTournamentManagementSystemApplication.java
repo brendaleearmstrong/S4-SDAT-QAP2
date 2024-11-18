6b6516c63c81b1dab4b2556a39593f9c90e9ba96
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-11-18 10:51:56 +0100

    Init Setup

---
+++ src/main/java/com/golfclub/golf_club_api/GolfClubTournamentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.golfclub.golf_club_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfClubTournamentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfClubTournamentManagementSystemApplication.class, args);
+	}
+
+}
