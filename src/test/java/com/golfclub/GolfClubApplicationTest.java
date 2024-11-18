11e53329dc0eb6b157fa3dc154747681e09be180
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-11-18 13:21:41 +0100

    Fix: Pom.xml dependeny issues

--- src/test/java/com/golfclub/GolfClubApplicationTest.java
+++ src/test/java/com/golfclub/GolfClubApplicationTest.java
@@ -1,14 +1,10 @@
-// src/test/java/com/golfclub/GolfClubApplicationTests.java
 package com.golfclub;
 
-import com.golfclub.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ContextConfiguration(classes = {TestConfig.class})
-class GolfClubApplicationTests {
+class GolfClubApplicationTest {
 
 	@Test
 	void contextLoads() {
