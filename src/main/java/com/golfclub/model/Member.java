3b01f36ae6813a7e8657fe69ab215d3a4d654ee3
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-11-18 13:58:36 +0100

    feat: Add Member entity with relationships

--- src/main/java/com/golfclub/model/Member.java
+++ src/main/java/com/golfclub/model/Member.java
@@ -1,4 +1,36 @@
 package com.golfclub.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "members")
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String memberName;
+
+    @Column(nullable = false)
+    private String memberAddress;
+
+    @Column(nullable = false, unique = true)
+    private String memberEmail;
+
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private Integer membershipDuration;
+
+    @ManyToMany(mappedBy = "participants")
+    private Set<Tournament> tournaments = new HashSet<>();
 }
